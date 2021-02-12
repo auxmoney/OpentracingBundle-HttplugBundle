e19aa077b5de7fe8942712a67521dba3791de395
Filis Futsarov <filisfutsarov@gmail.com>
2021-02-12 17:16:50 +0100

    push

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-rm -rf /package
-mkdir /package
+echo "inside requireadditionalvendors"
+#rm -rf /package
+#mkdir /package
 shopt -s extglob
-cp -r !(build|Tests) /package
+#cp -r !(build|Tests) /package
 
 cd build/testproject/
+#composer require auxmoney/opentracing-bundle-php-http-httplug-bundle
 
 # composer config extra.symfony.require "${SYMFONY_VERSION}"
 composer config extra.symfony.allow-contrib true
-composer config repositories.origin path /package
+#composer config repositories.origin path /package
 composer config use-github-api false
 
-
 composer require php-http/curl-client # virtual php-http/client-implementation
-composer require nyholm/psr7
+composer require http-interop/http-factory-guzzle # virtual psr/http-factory-implementation
+#composer require php-http/message
+#composer require http-interop/http-factory-guzzle
 composer require php-http/httplug-bundle
-composer require http-interop/http-factory-guzzle
-
 composer require auxmoney/opentracing-bundle-php-http-httplug-bundle
+
+
+composer fix-recipes
+rm -fr vendor/auxmoney/opentracing-bundle-php-http-httplug-bundle/*
+cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-php-http-httplug-bundle
+composer dump-autoload
+cd ../../
 
 # composer require symfony/flex --no-update # adds flex, suppress composer.lock
 # composer install                          # create a lock file
@@ -26,4 +34,4 @@ composer require auxmoney/opentracing-bundle-php-http-httplug-bundle
 # git reset --hard                          # reset tracked files
 # git clean -df                             # clean up recipe generated files and folders
 
-cd ../../
+#cd ../../
