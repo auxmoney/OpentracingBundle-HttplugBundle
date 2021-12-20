7a74fbfe0e4b720a5c83e10ac09042cf8956646b
Christian Alexander Wolf <cawolf@users.noreply.github.com>
2021-12-20 08:07:47 +0100

    feat: support PHP 8 (#5)

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -9,9 +9,14 @@ composer config use-github-api false
 
 composer require php-http/curl-client # virtual php-http/client-implementation
 composer require http-interop/http-factory-guzzle # virtual psr/http-factory-implementation
-composer require auxmoney/opentracing-bundle-php-http-httplug-bundle
 
-rm -rf vendor/auxmoney/opentracing-bundle-php-http-httplug-bundle/*
-cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-php-http-httplug-bundle
+VENDOR_VERSION=""
+CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
+CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
+if [[ $CURRENT_BRANCH -ne "master" ]]; then
+    composer config minimum-stability dev
+    VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
+fi
+composer require auxmoney/opentracing-bundle-php-http-httplug-bundle${VENDOR_VERSION}
 composer dump-autoload
 cd ../../
